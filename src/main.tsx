842ecb7ec92b03df9d13b7614c0d93c3ddbf4fc0
MarikaKonturova <mashakonturova@gmail.com>
2023-12-18 07:36:16 +0300

    proj init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import '@fontsource/amatic-sc/400.css';
+import '@fontsource/amatic-sc/700.css';
+import React from 'react'
+
+import ReactDOM from 'react-dom/client'
+import './styles/index.scss'
+
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
